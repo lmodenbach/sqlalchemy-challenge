8fffb9c6cfdca82d89de8f9b02331f650e38a947
lmodenbach <lmodenbach@gmail.com>
2021-05-15 20:08:19 -0700

    major progress

--- climate_app.py
+++ climate_app.py
@@ -25,15 +25,17 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"YOU CHOOSE START OF PERIOD<br/>"
+        f"/api/v1.0/start<br/>"
+        f"YOU CHOOSE START AND END OF PERIOD<br/>"
+        f"/api/v1.0/start/end"
         )
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     
-    results = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.station == "USC00519281").all()
+    results = session.query(Measurement.date, Measurement.prcp).all()
 
     precip = []    
     for date, prcp in results:
@@ -43,8 +45,75 @@ def precipitation():
         precip.append(precip_dict)
 
     return jsonify(precip)
-
     session.close()
-        
+       
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    
+    results = session.query(Station.name).all()
+    
+    return jsonify(results)
+    session.close()
+
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    
+    most_temps_and_station = session.query(Measurement.station, func.count(Measurement.tobs)).\
+                  group_by(Measurement.station).\
+                  order_by(func.count(Measurement.tobs).desc()).first()
+    
+    results = session.query(Measurement.tobs).\
+              filter(Measurement.station == most_temps_and_station[0]).all()
+
+    return jsonify(results)
+    session.close()
+
+
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    
+    min = session.query(func.min(Measurement.tobs)).\
+           filter(Measurement.date >= start).all()
+    
+    max = session.query(func.max(Measurement.tobs)).\
+           filter(Measurement.date >= start).all()
+
+    avg = session.query(func.avg(Measurement.tobs)).\
+           filter(Measurement.date >= start).all()
+
+    stats = [{"Min Temp: ":min}, {"Max Temp: ":max}, {"Avg Temp: ":avg}]    
+    
+    return jsonify(stats)
+    session.close()
+
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    
+    min = session.query(func.min(Measurement.tobs)).\
+           filter(Measurement.date >= start).\
+           filter(Measurement.date <= end).all()
+
+    max = session.query(func.max(Measurement.tobs)).\
+           filter(Measurement.date >= start).\
+           filter(Measurement.date <= end).all()
+
+    avg = session.query(func.avg(Measurement.tobs)).\
+           filter(Measurement.date >= start).\
+           filter(Measurement.date <= end).all()
+
+    stats = [{"Min Temp: ":min}, {"Max Temp: ":max}, {"Avg Temp: ":avg}]    
+    
+    return jsonify(stats)
+    session.close()
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
